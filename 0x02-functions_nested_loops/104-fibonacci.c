5ba6739a1a3e1c6150b894d9e46ef379e4f4817a
Mouniiiiiiii <monia.bousmara@gmail.com>
2023-06-21 17:20:59 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,7 +6,8 @@
  * Return: number of digits
  */
 
-int numlength(int num)
+int num
+ength(int num)
 {
 	int length = 0;
 
@@ -35,7 +36,7 @@ int main(void)
 	{
 		if (f1o > 0)
 			printf("%lu", f1o);
-		initial0s = numLength(mx) - 1 - numLength(f1);
+		initial0s = numlength(mx) - 1 - numlength(f1);
 		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%i", 0);
@@ -57,5 +58,6 @@ int main(void)
 		i++;
 	}
 	return (0);
+}
 
 

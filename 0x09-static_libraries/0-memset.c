419befd5ae17c9f6f0505130dbef8dcb52137e8f
Mouniiiiiiii <monia.bousmara@gmail.com>
2023-07-07 14:07:45 -0700

    libmy.a, main.h

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - program filles a block of mimory with a specific value
+ * @s: starting adress of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: change array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a = 0;
+
+	for (; n > 0; a++)
+	{
+		s[a] = b;
+		n--;
+	}
+	return (s);
+}
